ea69fadebdbece03fe78113c197e700725165a6f
Aphili734 <ajeesh_philip@comcast.com>
2023-03-08 11:46:55 +0530

    new files

---
+++ logical_with_if.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "secret" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
+fi
+
+
+echo "Enter any number"
+read n
+
+if [[ ( $n -eq 15 || $n  -eq 45 ) ]]
+then
+echo "You won the game"
+else
+echo "You lost the game"
+fi
+
+
